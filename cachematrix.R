92e9650921d19ca68a6bbf601d615bc4e89301fa
Charlotte Soneson <charlottesoneson@gmail.com>
2014-04-27 18:12:29 +0200

    Modified cachematrix.R, included the solution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script contains functions for computing the inverse 
+## of a square matrix and cache the result. 
+## If the inverse of the matrix has already been computed 
+## (and cached), it will return the pre-computed inverse.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "matrix object" (a list), 
+## that contains functions to set and get the matrix itself, 
+## as well as to set and get the inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  ## Create the function for setting the value of the matrix
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  ## Create the function returning the matrix
+  get <- function() x
+  
+  ## Create the function for setting the value of the inverse
+  setinv <- function(inverse) inv <<- inverse
+  
+  ## Create the function returning the inverse
+  getinv <- function() inv
+  
+  ## Return a list containing the constructed functions
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of x, which should 
+## be an object of the type returned from makeCacheMatrix.
+## If the inverse is already computed (and cached),
+## the pre-computed value is returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Get the inverse of x from the cache
+  inv <- x$getinv()
+  
+  ## If the inverse existed, return the cached value
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  ## If the inverse did not exist, get the matrix from x and 
+  ## compute the inverse
+  data <- x$get()
+  inv <- solve(data, ...)
+  
+  ## Cache the value of the inverse
+  x$setinv(inv)
+  
+  ## Return the inverse
+  inv
 }
